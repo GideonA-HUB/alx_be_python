d90b21f53e31d35612f4339b23a6ae924d3061b5
Gideon Amienghemhen <amienz0958@gmail.com>
2024-12-15 15:23:48 +0100

    commiting basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(addition_result))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtraction_result))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication_result))
